ec5d12d2c70eab7d2a6e923a50a88dd09a9a14a6
SwagJ <hashswan.j.m@gmail.com>
2019-07-04 18:44:45 +0200

    Update loss_opr.py

--- furnace/seg_opr/loss_opr.py
+++ furnace/seg_opr/loss_opr.py
@@ -9,40 +9,7 @@ from engine.logger import get_logger
 
 logger = get_logger()
 
-
-class SigmoidFocalLoss(nn.Module):
-    def __init__(self, ignore_label, gamma=2.0, alpha=0.25,
-                 reduction='mean'):
-        super(SigmoidFocalLoss, self).__init__()
-        self.ignore_label = ignore_label
-        self.gamma = gamma
-        self.alpha = alpha
-        self.reduction = reduction
-
-    def forward(self, pred, target):
-        b, h, w = target.size()
-        pred = pred.view(b, -1, 1)
-        pred_sigmoid = pred.sigmoid()
-        target = target.view(b, -1).float()
-        mask = (target.ne(self.ignore_label)).float()
-        target = mask * target
-        onehot = target.view(b, -1, 1)
-
-        max_val = (-pred_sigmoid).clamp(min=0)
-
-        pos_part = (1 - pred_sigmoid) ** self.gamma * (
-                pred_sigmoid - pred_sigmoid * onehot)
-        neg_part = pred_sigmoid ** self.gamma * (max_val + (
-                (-max_val).exp() + (-pred_sigmoid - max_val).exp()).log())
-
-        loss = -(self.alpha * pos_part + (1 - self.alpha) * neg_part).sum(
-            dim=-1) * mask
-        if self.reduction == 'mean':
-            loss = loss.mean()
-
-        return loss
-
-
+# Loss function used in BiSeNet
 class ProbOhemCrossEntropy2d(nn.Module):
     def __init__(self, ignore_label, reduction='mean', thresh=0.6, min_kept=256,
                  down_ratio=1, use_weight=False):
